0605d58f4e2d459a24ba0ca991e65aebc3f5f934
yashLadha <201551061@iiitvadodara.ac.in>
2017-11-17 15:11:41 +0530

    Get all orders
    Signed-off-by: yashLadha <201551061@iiitvadodara.ac.in>

--- routes/orders.js
+++ routes/orders.js
@@ -4,14 +4,23 @@ let router = express.Router();
 let firestore = require('../helpers/firestoreHelper');
 
 /**
- * API Endpoint for accepting orders
- * by a seller havng id : uid
+ * API Endpoint for getting all the orders
+ * for the seller
  */
-router.post('/accept', (req, res, next) => {
-    let uid = req.body.uid;
-    let orderId = req.body.orderId;
-    
-    let sellerOrderRef = firestore.doc(uid + '/orders/waiting/' + orderId);
+router.get('/all/:uid', (req ,res, next) => {
+    let uid = req.params.uid
+    let orderRef = firestore.collection(uid + '/orders/waiting')
+
+    orderRef.get()
+        .then(snapShot => {
+            let response = {}
+            snapShot.forEach(item =>{
+                console.log(item.id, '=>', item.data());
+                response[item.id] = item.data()
+            })
+            res.json(response)
+        })
+        .catch(err => res.json({response: 500}))
 })
 
 module.exports = router;
